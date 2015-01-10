d6bb11ffb2afbfd069a7014472bbc627454a4a52
Kyle Harrison <kylejharrison@gmail.com>
2015-01-10 13:04:59 +0000

    edited fix internet scripts

--- fix_internet-study.sh
+++ fix_internet-study.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
+netctl disable wlp2s0-VM535068-2G
+netctl disable my-network
 netctl reenable  wlp2s0-devolo-f4068d0e1a92
-netctl reenable my-network
 netctl restart wlp2s0-devolo-f4068d0e1a92
-netctl restart my-network
