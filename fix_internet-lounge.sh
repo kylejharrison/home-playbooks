da9c0b4450ddeaffe1b767fb11fbc8e126cd45ea
Kyle Harrison <kylejharrison@gmail.com>
2015-01-31 17:15:13 +0000

    Revert "Merge branch 'master' of github.com:kylejharrison/home-playbooks"
    This reverts commit de5682aa9d88689102ba43a7a6432435b9bb19df, reversing
    changes made to b66670f3a86083fe33b87297f6eab7a427647ed9.

--- fix_internet-lounge.sh
+++ fix_internet-lounge.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-wpa_supplicant -B -i wlp2s0 -c <(wpa_passphrase "VM535068-5G" "venvejma")
-dhcpcd wlp2s0
+
+netctl disable wlp2s0-VM535068-2G
+netctl disable wlp2s0-devolo-f4068d0e1a92
+netctl reenable my-network
+netctl restart my-network
